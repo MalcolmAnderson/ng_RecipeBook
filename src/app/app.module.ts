6e78efb25250bf7ff2a43169a991070afcff47b7
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-10-24 08:02:34 -0700

    start over again, following along

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipeDetailComponent } from './recipeBook/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipeBook/recipe-item/recipe-item.component';
-import { RecipeListComponent } from './recipeBook/recipe-list/recipe-list.component';
-import { ShoppingListComponent } from './shoppingList/shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shoppingList/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    RecipeItemComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    ShoppingListComponent,
-    ShoppingListEditComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule
