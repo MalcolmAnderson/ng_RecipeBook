e3cec5d095f1e87c7501c604414a5f10a626d292
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-10-23 20:42:50 -0700

    Dead-end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { RecipeListComponent } from './recipeBook/recipe-list/recipe-list.component';
+import { AppHeader } from './app-header/app-header.component';
+import { RecipeDetailComponent } from './recipeBook/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipeBook/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipeBook/recipe-list/recipe-list.component';
+import { ShoppingListComponent } from './shoppingList/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shoppingList/shopping_list_edit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AppHeader,
+    RecipeItemComponent,
     RecipeListComponent,
-    RecipeItemComponent
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent
   ],
   imports: [
     BrowserModule
