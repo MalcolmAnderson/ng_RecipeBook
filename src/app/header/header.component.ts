9bc32488357f3abf534ee05f1f33b34373c53efc
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-10-26 17:38:22 -0700

    Create recipe.model.ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  constructor() {
+    let collapsed: boolean = true;
+  }
 
   ngOnInit(): void {
   }
