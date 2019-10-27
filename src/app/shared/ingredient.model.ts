0fd4c2cc4c2208b0e8c9d792834e4bcaee28f4ac
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-10-26 22:48:54 -0700

    57 create and output shopping list

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,7 @@
+export class Ingredient {
+  constructor(
+    public name: string,
+    public type: string,
+    public amount: number,
+    public units: string) {}
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/Ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients = [];
+  ingredients: Ingredient[] = [
+    new Ingredient('Tomatoes', 'Diced', 2, 'Cups'),
+    new Ingredient('Apples', 'Honey Crisp', 12, 'Whole')
+  ];
 
   constructor() { }
 
